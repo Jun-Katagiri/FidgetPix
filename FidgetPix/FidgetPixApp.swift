20bef4a7d2370d48c1c50b8b20eb13ae8357e557
Jun-Katagiri <jun@katagiri.cc>
2026-04-09 15:58:59 +0900

    Initial Commit

---
+++ FidgetPix/FidgetPixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetPixApp.swift
+//  FidgetPix
+//
+//  Created by Jun Katagiri on 2026/04/09.
+//
+
+import SwiftUI
+
+@main
+struct FidgetPixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
